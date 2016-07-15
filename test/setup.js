947d073425a3fdd75390605ff34a959b79b9ccb8
Max P <maxfarseer@gmail.com>
2016-07-15 13:45:28 +0300

    fix setup for tests

--- test/setup.js
+++ test/setup.js
@@ -3,7 +3,6 @@ import React from 'react' // eslint-disable-line no-unused-vars
 import chai, { expect } from 'chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-//import chaiAsPromised from 'chai-as-promised'
 import { shallow, mount, render } from 'enzyme'
 import chaiEnzyme from 'chai-enzyme'
 import { jsdom } from 'jsdom'
@@ -11,7 +10,6 @@ import { jsdom } from 'jsdom'
 require('mock-local-storage')
 
 chai.use(sinonChai)
-//chai.use(chaiAsPromised)
 chai.use(chaiEnzyme())
 
 global.document = jsdom('')
@@ -20,7 +18,4 @@ global.navigator = { userAgent: 'browser' }
 
 global.React = React
 global.expect = expect
-
-global.fdescribe = (...args) => describe.only(...args)
-global.fit = (...args) => it.only(...args)
 /* eslint-enable */
